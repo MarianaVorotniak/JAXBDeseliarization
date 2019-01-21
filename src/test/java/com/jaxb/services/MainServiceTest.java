c47454a0e51e2650eea839a97d3ed86cea71217f
Mariana <mvoro@softserveinc.com>
2019-01-21 18:10:32 +0200

    added tests, new functionality for reading soapfaults and refactored code

--- src/test/java/com/jaxb/services/MainServiceTest.java
+++ src/test/java/com/jaxb/services/MainServiceTest.java
@@ -1,6 +1,7 @@
 package com.jaxb;
 
 import com.jaxb.exceptions.ParseException;
+import com.jaxb.services.MainService;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -11,27 +12,31 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 
 @RunWith(MockitoJUnitRunner.class)
-public class MainTest {
+public class MainServiceTest {
 
     @Rule
     public ExpectedException expectedEx = ExpectedException.none();
 
     @Test
     public void translateTest() throws ParseException {
-        String actualCorrectTranslated = Main.translate("Aceptacion Completa");
-        assertEquals("Fully accepted", actualCorrectTranslated);
-        assertNotEquals("Accepted", actualCorrectTranslated);
+        String actualAcceptanceTranslated = MainService.translate("Aceptacion Completa");
+        assertEquals("Fully accepted", actualAcceptanceTranslated);
+        assertNotEquals("Accepted", actualAcceptanceTranslated);
+
+        String actualRejectedTranslated = MainService.translate("Rechazo Completo");
+        assertEquals("Rejected", actualRejectedTranslated);
+        assertNotEquals("Accepted", actualRejectedTranslated);
 
         expectedEx.expect(ParseException.class);
         expectedEx.expectMessage("Word is not a correct status");
-        String actualWrongTranslated = Main.translate("Aceptacion");
+        String actualWrongTranslated = MainService.translate("Aceptacion");
     }
 
     @Test
     public void readFileTest() throws ParseException {
         expectedEx.expect(ParseException.class);
         expectedEx.expectMessage("File does not exist");
-        String fileContent = Main.readFile("file");
+        String fileContent = MainService.readFile("file");
     }
 
 
